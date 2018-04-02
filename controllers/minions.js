fe0a4dae4b862e701fd65e2942260d2d3b31269c
Philippe Luchansky <philuchansky@gmail.com>
2018-04-01 23:48:52 -0700

    Minion model

--- controllers/minions.js
+++ controllers/minions.js
@@ -1,5 +1,9 @@
+const Minion = require('../models/Minion.js')
+
 module.exports = {
   index: (req, res) => {
-    res.json({ message: "Minions API root." })
+    Minion.find({}, (err, allTheMinions) => {
+      res.json(allTheMinions)
+    })
   }
 }
---
+++ models/Minion.js
@@ -0,0 +1,9 @@
+const
+  mongoose = require('mongoose'),
+  minionSchema = new mongoose.Schema({
+    name: String,
+    imageUrl: { type: String, default: " https://goo.gl/u6GHrh"}
+  })
+
+const Minion = mongoose.model('Minion', minionSchema)
+module.exports = Minion
