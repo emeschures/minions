408e7c8e4a577ba7d15e27dbc53aec9768b50fec
Philippe Luchansky <philuchansky@gmail.com>
2018-04-01 23:52:37 -0700

    minion controller actions

--- controllers/minions.js
+++ controllers/minions.js
@@ -5,5 +5,29 @@ module.exports = {
     Minion.find({}, (err, allTheMinions) => {
       res.json(allTheMinions)
     })
+  },
+
+  create: (req, res) => {
+    Minion.create(req.body, (err, savedMinion) => {
+      res.json({ success: true, message: "Minion created.", minion: savedMinion })
+    })
+  },
+
+  show: (req, res) => {
+    Minion.findById(req.params.id, (err, thatMinion) => {
+      res.json(thatMinion)
+    })
+  },
+
+  update: (req, res) => {
+    Minion.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedMinion) => {
+      res.json({ success: true, message: "Minion updated.", minion: updatedMinion })
+    })
+  },
+
+  destroy: (req, res) => {
+    Minion.findByIdAndRemove(req.params.id, (err, deletedMinion) => {
+      res.json({ success: true, message: "Minion deleted." })
+    })
   }
 }
