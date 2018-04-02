1bacd7f0c73f8803149c172cc6d22215137313f2
Philippe Luchansky <philuchansky@gmail.com>
2018-04-01 23:54:13 -0700

    apply minion routes with actions, fix spelling of default imageUrl

--- models/Minion.js
+++ models/Minion.js
@@ -2,7 +2,7 @@ const
   mongoose = require('mongoose'),
   minionSchema = new mongoose.Schema({
     name: String,
-    imageUrl: { type: String, default: " https://goo.gl/u6GHrh"}
+    imageUrl: { type: String, default: "https://goo.gl/u6GHrh"}
   })
 
 const Minion = mongoose.model('Minion', minionSchema)
--- routes/minions.js
+++ routes/minions.js
@@ -5,5 +5,11 @@ const
 
 minionsRouter.route('/')
   .get(minionsCtrl.index)
+  .post(minionsCtrl.create)
+
+minionsRouter.route('/:id')
+  .get(minionsCtrl.show)
+  .patch(minionsCtrl.update)
+  .delete(minionsCtrl.destroy)
 
 module.exports = minionsRouter
